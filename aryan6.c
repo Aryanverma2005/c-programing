0437d719ace1ba7a4c20afb7f41104a327bd2c5d
Aryanverma2005 <92083434+Aryanverma2005@users.noreply.github.com>
2021-10-08 15:41:44 +0530

    Add files via upload

---
+++ aryan6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int dividend, divisor,quotient, remainder;
+printf("enter dividend:");
+scanf("%d", &dividend);
+printf("enter divisor:");
+scanf("%d", &divisor);
+// compute quotient
+quotient= dividend/divisor;
+// compute remainder
+remainder= dividend % divisor;
+printf("quotient=%d\n", quotient);
+printf("remainder= %d", remainder);
+return 0;
+}
+
