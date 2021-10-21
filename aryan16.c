d1b2c61ebfa0635fe081a17f827e6e844a06d7e4
Aryanverma2005 <92083434+Aryanverma2005@users.noreply.github.com>
2021-10-21 11:07:28 +0530

    Add files via upload

---
+++ aryan16.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int x,y,sum,substract,multiply,devide,mod;
+x=16;
+y=5;
+sum=x+y;
+substract=x-y;
+multiply=x*y;
+devide=x/y;
+mod= x%y;
+printf("%d\n%d\n%d\n%d\n%d",sum,substract,multiply,devide,mod);
+return 0;
+}
+
