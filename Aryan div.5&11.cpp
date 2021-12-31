c014adbed295998bd677951db7df32109b9ee7be
Aryanverma2005 <92083434+Aryanverma2005@users.noreply.github.com>
2021-12-31 22:31:05 +0530

    Add files via upload

---
+++ Aryan div.5&11.cpp
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main()
+{
+	int x;
+	scanf("%d",&x);
+	if(x%5==0&x%11==0)
+	printf("no. is divisible by 5 & 11 ");
+	else
+	printf("no. is not divisible by 5 & 11 ");
+    return 0;
+}
+
