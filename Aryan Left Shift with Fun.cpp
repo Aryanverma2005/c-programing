46bf60b3762e925e5d81f862175eb7a9bd52e572
Aryanverma2005 <92083434+Aryanverma2005@users.noreply.github.com>
2022-01-15 22:46:51 +0530

    Add files via upload

---
+++ Aryan Left Shift with Fun.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void LeftShift(void)
+{
+	int x,y,n;
+	printf("Enter the value of digit = %d",x);
+	 scanf("%d",&x);
+	printf("Enter the value of Left Shift value = %d",n);
+	scanf("%d",&n);
+	y=x<<n;
+	printf("%d",y);
+}
+int main()
+{
+	int LeftShift();
+	LeftShift();
+    printf("%d",LeftShift);
+    return 0;
+}
