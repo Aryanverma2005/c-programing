d49d36f5d72c7f05089b869bdbe185049018582d
Aryanverma2005 <92083434+Aryanverma2005@users.noreply.github.com>
2022-01-15 22:46:25 +0530

    Add files via upload

---
+++ Aryan Right Shift with fun.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void RightShift(void)
+{
+	int x,y,n;
+	printf("Enter the value of digit = %d",x);
+	 scanf("%d",&x);
+	printf("Enter the value of Right Shift value = %d",n);
+	scanf("%d",&n);
+	y=x>>n;
+	printf("%d",y);
+}
+int main()
+{
+	int RightShift();
+	RightShift();
+    printf("%d",RightShift);
+    return 0;
+}
